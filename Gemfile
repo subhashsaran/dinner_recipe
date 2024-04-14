5fa31d764063f99cd4158505d0248a35202a36a1
Subhash <subhash@Subhashs-MacBook-Pro-2.local>
2024-04-14 12:43:29 +0200

    Add Rspec and FactoryBot for testing

--- Gemfile
+++ Gemfile
@@ -70,4 +70,9 @@ group :test do
   gem "capybara"
   gem "selenium-webdriver"
   gem "webdrivers"
+  gem "rspec-rails"
+  gem "factory_bot_rails"
+  gem 'shoulda-matchers'
+  gem 'rspec-expectations'
+  gem 'rails-controller-testing'
 end
--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,17 @@ module DinnerRecipe
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
+    config.generators do |g|
+      g.test_framework :rspec,
+        fixtures: true,
+        view_specs: false,
+        helper_specs: false,
+        routing_specs: false,
+        controller_specs: true,
+        request_specs: true
+      g.fixture_replacement :factory_bot, dir: 'spec/factories'
+    end
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
